f811964df34a1cc5b805ca4eb700f8ae52d4442d
Enio Nascimento <enio.analista@gmail.com>
2024-12-07 16:06:39 -0300

    first commit

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Cabecalho.module.css'
+import { Header } from './styles'
+
+const Cabecalho = () => (
+  <Header>
+    <h1>EBAC Jobs</h1>
+  </Header>
+)
+
+export default Cabecalho
---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,28 @@
+import { FormEvent, useState } from 'react'
+
+import { Botao, Form_f, Input } from './styles'
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Form_f onSubmit={aoEnviarForm}>
+      <Input
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <Botao>Pesquisar</Botao>
+    </Form_f>
+  )
+}
+export default FormVagas
